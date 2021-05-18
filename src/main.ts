c13778bd7c8dda8f1ea9dcb9206f7232b313c922
ricardo <ricardo@inuits.eu>
2021-05-18 11:23:01 +0200

    package lock adjustemts and docker folder

--- src/main.ts
+++ src/main.ts
@@ -28,41 +28,41 @@ let configFile = {
 
 configFile = new MasterConfig().deserialize(configFile)
 
-if (configFile.oidc && configFile.oidc.baseUrl && configFile.oidc.clientId) {
-  app.use(OpenIdConnectPlugin, {
-    store: store,
-    router: router,
-    configuration: {
-      baseUrl: configFile.oidc.baseUrl,
-      serverBaseUrl: configFile.oidc.serverBaseUrl,
-      tokenEndpoint: configFile.oidc.tokenEndpoint ? configFile.oidc.tokenEndpoint : 'token',
-      authEndpoint: configFile.oidc.authEndpoint ? configFile.oidc.authEndpoint : 'auth',
-      logoutEndpoint: configFile.oidc.logoutEndpoint ? configFile.oidc.logoutEndpoint : 'logout',
-      clientId: configFile.oidc.clientId,
-      authorizedRedirectRoute: '/',
-      serverTokenEndpoint: 'token/',
-      serverRefreshEndpoint: 'refresh/',
-      InternalRedirectUrl: '',
-    },
-  })
-}
+// if (configFile.oidc && configFile.oidc.baseUrl && configFile.oidc.clientId) {
+//   app.use(OpenIdConnectPlugin, {
+//     store: store,
+//     router: router,
+//     configuration: {
+//       baseUrl: configFile.oidc.baseUrl,
+//       serverBaseUrl: configFile.oidc.serverBaseUrl,
+//       tokenEndpoint: configFile.oidc.tokenEndpoint ? configFile.oidc.tokenEndpoint : 'token',
+//       authEndpoint: configFile.oidc.authEndpoint ? configFile.oidc.authEndpoint : 'auth',
+//       logoutEndpoint: configFile.oidc.logoutEndpoint ? configFile.oidc.logoutEndpoint : 'logout',
+//       clientId: configFile.oidc.clientId,
+//       authorizedRedirectRoute: '/',
+//       serverTokenEndpoint: 'token/',
+//       serverRefreshEndpoint: 'refresh/',
+//       InternalRedirectUrl: '',
+//     },
+//   })
+// }
 
-store.dispatch('setConfig', configFile)
-router.beforeEach((to: any, from: any, next: any) => {
-  if (to.matched.some((record: any) => record.meta.requiresOpenIdAuth)) {
-    if (store.getters.isLoggedIn) {
-      !store.getters.loaded ? RepositoryFactory.get(AuthRepository).me().then((user: any) => {
-        store.dispatch('setUser', user.result).then(() => {
-          next()
-        })
-      }) : next()
-    } else {
-      next()
-    }
-  } else {
-    next()
-  }
-})
+// store.dispatch('setConfig', configFile)
+// router.beforeEach((to: any, from: any, next: any) => {
+//   if (to.matched.some((record: any) => record.meta.requiresOpenIdAuth)) {
+//     if (store.getters.isLoggedIn) {
+//       !store.getters.loaded ? RepositoryFactory.get(AuthRepository).me().then((user: any) => {
+//         store.dispatch('setUser', user.result).then(() => {
+//           next()
+//         })
+//       }) : next()
+//     } else {
+//       next()
+//     }
+//   } else {
+//     next()
+//   }
+// })
 
 app.use(router).use(store).mount('#app')
 // })
