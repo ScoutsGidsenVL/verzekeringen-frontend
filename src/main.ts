34b7615669f897b478b473b0e23ff729e3d92396
ricardo <ricardo@inuits.eu>
2021-05-27 11:33:03 +0200

    Adjusted config file

--- src/main.ts
+++ src/main.ts
@@ -7,62 +7,55 @@ import MasterConfig from './models/config/masterConfig'
 import { OpenIdConnectPlugin } from 'inuits-vuejs-oidc'
 import RepositoryFactory from './repositories/repositoryFactory'
 import AuthRepository from './repositories/authRepository'
+import StaticFileRepository from './repositories/staticFileRepository'
 
-// new StaticFileRepository().getFile('config.json').then((configFile: any) => {
-const app = createApp(App)
+new StaticFileRepository().getFile('config.json').then((result: any) => {
+  const app = createApp(App)
 
-let configFile = {
-  api: {
-    baseUrl: 'http://localhost:8011',
-    apiSuffix: 'api/',
-  },
-  oidc: {
-    baseUrl: 'https://idp-dev.inuits.io/auth/realms/scouts-dev',
-    serverBaseUrl: 'http://localhost:8011/api/oidc',
-    authEndpoint: 'protocol/openid-connect/auth',
-    logoutEndpoint: 'protocol/openid-connect/logout',
-    clientId: 'scouts-workflows',
-    tokenEndpoint: '',
-  },
-}
+  let configFile = result
 
-configFile = new MasterConfig().deserialize(configFile)
+  configFile = new MasterConfig().deserialize(configFile)
 
-// if (configFile.oidc && configFile.oidc.baseUrl && configFile.oidc.clientId) {
-//   app.use(OpenIdConnectPlugin, {
-//     store: store,
-//     router: router,
-//     configuration: {
-//       baseUrl: configFile.oidc.baseUrl,
-//       serverBaseUrl: configFile.oidc.serverBaseUrl,
-//       tokenEndpoint: configFile.oidc.tokenEndpoint ? configFile.oidc.tokenEndpoint : 'token',
-//       authEndpoint: configFile.oidc.authEndpoint ? configFile.oidc.authEndpoint : 'auth',
-//       logoutEndpoint: configFile.oidc.logoutEndpoint ? configFile.oidc.logoutEndpoint : 'logout',
-//       clientId: configFile.oidc.clientId,
-//       authorizedRedirectRoute: '/',
-//       serverTokenEndpoint: 'token/',
-//       serverRefreshEndpoint: 'refresh/',
-//       InternalRedirectUrl: '',
-//     },
-//   })
-// }
+  if (configFile.oidc && configFile.oidc.baseUrl && configFile.oidc.clientId) {
+    // @ts-ignore
+    app.use(OpenIdConnectPlugin, {
+      store: store,
+      router: router,
+      configuration: {
+        baseUrl: configFile.oidc.baseUrl,
+        serverBaseUrl: configFile.oidc.serverBaseUrl,
+        authEndpoint: configFile.oidc.authEndpoint ? configFile.oidc.authEndpoint : 'auth',
+        logoutEndpoint: configFile.oidc.logoutEndpoint ? configFile.oidc.logoutEndpoint : 'logout',
+        clientId: configFile.oidc.clientId,
+        authorizedRedirectRoute: '/',
+        serverTokenEndpoint: 'token/',
+        serverRefreshEndpoint: 'refresh/',
+        InternalRedirectUrl: '',
+      },
+    })
+  }
 
-// store.dispatch('setConfig', configFile)
-// router.beforeEach((to: any, from: any, next: any) => {
-//   if (to.matched.some((record: any) => record.meta.requiresOpenIdAuth)) {
-//     if (store.getters.isLoggedIn) {
-//       !store.getters.loaded ? RepositoryFactory.get(AuthRepository).me().then((user: any) => {
-//         store.dispatch('setUser', user.result).then(() => {
-//           next()
-//         })
-//       }) : next()
-//     } else {
-//       next()
-//     }
-//   } else {
-//     next()
-//   }
-// })
+  store.dispatch('setConfig', configFile)
+  router.beforeEach((to: any, from: any, next: any) => {
+    if (to.matched.some((record: any) => record.meta.requiresOpenIdAuth)) {
+      if (store.getters.isLoggedIn) {
+        !store.getters.loaded
+          ? RepositoryFactory.get(AuthRepository)
+              .me()
+              .then((user: any) => {
+                store.dispatch('setUser', user.result).then(() => {
+                  next()
+                })
+              })
+              .catch(() => {})
+          : next()
+      } else {
+        next()
+      }
+    } else {
+      next()
+    }
+  })
 
-app.use(router).use(store).mount('#app')
-// })
+  app.use(router).use(store).mount('#app')
+})
