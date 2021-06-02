3e6d250bf4126b865b4b834ffe055aeb62f4aec1
ricardo <ricardo@inuits.eu>
2021-06-02 17:34:05 +0200

    Ref #78031, Base sidebar implemented

--- src/components/insurances/nonMembersInsurance/nonMembersList.vue
+++ src/components/insurances/nonMembersInsurance/nonMembersList.vue
@@ -7,26 +7,33 @@
   <div class="mt-2 cursor-pointer text-lightGreen" @click="openSideBar()">
     <strong> + voeg niet-lid toe </strong>
   </div>
+  <base-side-bar v-model:isDisplay="isDisplay" />
 </template>
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import NonMemberItem from '@/components/insurances/nonMembersInsurance/nonMemberItem.vue'
+import BaseSideBar from '@/components/semantic/BaseSideBar.vue'
+
 export default defineComponent({
   name: 'NonMembersList',
   components: {
     'non-member-item': NonMemberItem,
+    'base-side-bar': BaseSideBar,
   },
   props: {},
   setup() {
     const nonMembers = ref<Array<any>>([])
+    const isDisplay = ref<boolean>(false)
 
     const openSideBar = () => {
-      console.log('OPEN SIDEBAR')
+      console.log('openSideBar')
+      isDisplay.value = true
     }
 
     return {
       openSideBar,
       nonMembers,
+      isDisplay,
     }
   },
 })
---
+++ src/components/semantic/BaseSideBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="isDisplay" class="h-full w-full fixed top-0 left-0 overflow-x-hidden z-0 bg-black bg-opacity-20" @click="hideSideBar()"></div>
+  <div v-if="isDisplay" class="bg-white fixed top-0 right-0 h-full z-20 w-3/12 pt-5 px-3">
+    <div class="bg-red">{{ title }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { boolean } from 'yup'
+
+export default defineComponent({
+  name: 'BaseSideBar',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    isDisplay: boolean,
+  },
+  setup(props, context) {
+    const hideSideBar: () => void = () => {
+      context.emit('update:isDisplay', false)
+    }
+
+    return {
+      hideSideBar,
+    }
+  },
+})
+</script>
