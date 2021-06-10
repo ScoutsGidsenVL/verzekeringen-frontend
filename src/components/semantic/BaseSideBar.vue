04177bbca7d7996b5535c1d6bf9bbd34aebc8c18
ricardo <ricardo@inuits.eu>
2021-06-10 17:19:50 +0200

    Added button to close sidebar

--- src/components/semantic/BaseSideBar.vue
+++ src/components/semantic/BaseSideBar.vue
@@ -1,6 +1,13 @@
 <template>
   <div v-if="isDisplay" class="h-full w-full fixed top-0 left-0 overflow-x-hidden z-0 bg-black bg-opacity-20" @click="hideSideBar()"></div>
   <div v-if="isDisplay" :class="'bg-white fixed top-0 right-0 h-full z-20 pt-5 px-4' + ' ' + width">
+    <div class="mb-3 cursor-pointer" @click="hideSideBar()">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+        <path
+          d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"
+        />
+      </svg>
+    </div>
     <header-2 :text="title" />
     <form v-if="options" action="">
       <div class="flex gap-7">
