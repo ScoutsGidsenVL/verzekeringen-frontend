35938fba51e5474075d3c3336a0db0bf9f1a72a1
ricardo <ricardo@inuits.eu>
2021-06-15 14:16:45 +0200

    changed router options

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import RequestInsurance from '../views/RequestInsurance.vue'
 import oneTimeActivityDetail from '@/components/insurances/oneTimeActivityInsurance/oneTimeActivityDetail.vue'
@@ -102,7 +102,7 @@ const routes: any[] = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
