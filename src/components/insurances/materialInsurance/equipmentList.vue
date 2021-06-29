804a2bb7bff25438e92c15b9c127d470a99767bf
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-06-29 16:38:56 +0200

    update equipement item

--- src/components/insurances/materialInsurance/equipmentList.vue
+++ src/components/insurances/materialInsurance/equipmentList.vue
@@ -1,31 +1,31 @@
 <template>
-  <div class="grid grid-cols-2 gap-1" style="width: 850px">
-    <div v-if="equipment.length > 0" class="w-96">
-      <hr class="border-t-2 border-black" />
-    </div>
-    <div v-if="equipment.length > 1" class="w-96">
-      <hr class="border-t-2 border-black" />
-    </div>
-  </div>
-
-  <div class="grid grid-cols-2 gap-1" style="width: 850px">
-    <div class="w-96" v-for="(material, index) in equipment" :key="material.id">
+  <div class="grid lg:grid-cols-2 gap-1">
+    <div v-for="(material, index) in equipment" :key="material.id" class="w-full text-center">
       <equipment-item :equipment="material">
-        <template v-slot:top>
-          <div class="mt-3 flex justify-end" @click="editEquipment(material)">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-lightGreen cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-            </svg>
-          </div>
+        <template #top>
           <div>
             <div v-if="material.ownerMember">(Equipment van lid)</div>
             <div v-if="material.ownerNonMember">(Equipment van niet-lid)</div>
             <div v-if="!material.ownerNonMember && !material.ownerMember">(Equipment van groep)</div>
           </div>
         </template>
-
-        <div v-if="canBeDeleted" class="text-right">
-          <label @click="deleteEquipment(index)" class="hover:text-lightGreen cursor-pointer" for="">Verwijder</label>
+        <div v-if="canBeDeleted" class="text-left">
+          <a class="hover:text-lightGreen cursor-pointer link-inline inline-block mr-3" for="" @click="editEquipment(index)">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-lightGreen cursor-pointer inline-block mt-n1 mr-0" viewBox="0 0 20 20" fill="currentColor">
+              <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" /></svg
+            ><span class="inline-block">Bewerken</span></a
+          >
+          <a class="hover:text-lightGreen cursor-pointer link-inline" for="" @click="deleteEquipment(index)">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-lightGreen cursor-pointer inline-block mt-n1 mr-n1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+            Verwijder</a
+          >
         </div>
       </equipment-item>
     </div>
