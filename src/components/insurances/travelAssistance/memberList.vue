524f3a8ff1e890fc627602977dc2846b014f5cdf
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-06-29 16:39:03 +0200

    update member item

--- src/components/insurances/travelAssistance/memberList.vue
+++ src/components/insurances/travelAssistance/memberList.vue
@@ -2,8 +2,18 @@
   <div class="grid lg:grid-cols-2 gap-1">
     <div v-for="(member, index) in members" :key="member.id" class="w-96">
       <member-item :member="member">
-        <div v-if="canBeDeleted" class="text-right">
-          <label class="hover:text-lightGreen cursor-pointer" for="" @click="deleteMemberFromList(index)">Verwijder</label>
+        <div v-if="canBeDeleted" class="text-left">
+          <a class="hover:text-lightGreen cursor-pointer link-inline" for="" @click="deleteMemberFromList(index)">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 hover:text-lightGreen cursor-pointer inline-block mt-n1 mr-n1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+              />
+            </svg>
+            Verwijder</a
+          >
         </div>
       </member-item>
     </div>
