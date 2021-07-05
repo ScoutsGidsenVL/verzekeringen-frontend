5257713bf914479655cde1bbeef953636b15dc4e
ricardo <ricardo@inuits.eu>
2021-07-05 12:10:45 +0200

    Ref #78449 Basic multi select implemented for group in homescreen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,19 @@
   <div class="container">
     <div v-if="drafts">
       <custom-list :is-draft="true" :items="drafts.results" title="Nog te voltooien" @removeDraft="removeDraft($event)" />
+
       <div class="flex gap-5 pt-1 pb-5 float-right">
         <a v-if="drafts.previous" class="link-inline cursor-pointer" @click="getPreviousDrafts(drafts.previous)">Vorige pagina</a>
         <a v-if="drafts.next" class="link-inline cursor-pointer" @click="getNextDrafts(drafts.next)">Volgende pagina</a>
       </div>
     </div>
     <div v-if="data">
-      <custom-list :items="data.results" title="Recent aangevraagd" />
+      <custom-list :items="data.results" title="Recent aangevraagd">
+        <div class="w-80 pb-3">
+          <multi-select id="group" :object="true" placeholder="Filter op groep" track-by="fullInfo" value-prop="id" :options="userData.scoutsGroups" :loading-submit="isSubmitting" />
+        </div>
+      </custom-list>
+
       <div class="flex gap-5 pt-1 pb-5 float-right">
         <a v-if="data.previous" class="link-inline cursor-pointer" @click="getPreviousInsurances(data.previous)">Vorige pagina</a>
         <a v-if="data.next" class="link-inline cursor-pointer" @click="getNextInsurances(data.next)">Volgende pagina</a>
@@ -21,9 +27,11 @@
 <script lang="ts">
 import { InsuranceRepository } from '@/repositories/insurances/insuranceRepository'
 import { DraftRepository } from '@/repositories/insurances/draftRepository'
+import CallToAction from '../components/customHeadlines/CallToAction.vue'
+import { ResponsibleMember } from '@/serializer/ResponsibleMember'
 import RepositoryFactory from '@/repositories/repositoryFactory'
 import customList from '../components/semantic/CustomList.vue'
-import CallToAction from '../components/customHeadlines/CallToAction.vue'
+import MultiSelect from '@/components/inputs/MultiSelect.vue'
 import { ArrayResult } from '@/serializer/ArrayResult'
 import { defineComponent, ref } from 'vue'
 import { useStore } from 'vuex'
@@ -31,15 +39,16 @@ import { useStore } from 'vuex'
 export default defineComponent({
   name: 'Home',
   components: {
-    'custom-list': customList,
     'call-to-action': CallToAction,
+    'multi-select': MultiSelect,
+    'custom-list': customList,
   },
   setup: () => {
     const store = useStore()
     store.dispatch('resetStates')
-
     const data = ref<ArrayResult>()
     const drafts = ref<ArrayResult>()
+    const userData = ref<ResponsibleMember>(store.getters.user)
 
     const getInsurances = () => {
       RepositoryFactory.get(InsuranceRepository)
@@ -102,6 +111,7 @@ export default defineComponent({
       getPreviousDrafts,
       getNextDrafts,
       removeDraft,
+      userData,
       drafts,
       data,
     }
