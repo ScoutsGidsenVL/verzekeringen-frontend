e9655d314e3eec0401790d80fdbbc71b7c1a8757
ricardo <ricardo@inuits.eu>
2021-07-06 16:24:27 +0200

    Ref #79151, resolved feedback

--- src/components/inputs/CustomInput.vue
+++ src/components/inputs/CustomInput.vue
@@ -16,7 +16,6 @@
       }"
     >
       <input
-        oninvalid="this.setCustomValidity('De verzekering moet minstens 1 dag op voorhand aangevraagd worden.')"
         v-if="type !== InputTypes.TEXT_AREA && !hideInput && type !== InputTypes.TIME"
         v-model="inputValue"
         class="bg-lightGray p-2 min-w-0 w-100"
