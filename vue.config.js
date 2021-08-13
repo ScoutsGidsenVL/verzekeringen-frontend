93254b8dac54f9c945ca40a6b22cc777ea547aaa
ricardo <ricardo@inuits.eu>
2021-08-13 14:12:24 +0200

    Ref #80763, Using toast from library

--- vue.config.js
+++ vue.config.js
@@ -17,6 +17,11 @@ module.exports = {
       symlinks: false,
       alias: {
         vue: path.resolve("./node_modules/vue"),
+        "vee-validate": path.resolve("./node_modules/vee-validate"),
+        "@vee-validate/i18n": path.resolve("./node_modules/@vee-validate/i18n"),
+        "@vee-validate/rules": path.resolve(
+          "./node_modules/@vee-validate/rules"
+        ),
       },
     },
   },
