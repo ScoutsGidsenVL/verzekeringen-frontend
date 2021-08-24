a82c9cd955fd007aef9895df6521e68c6b09610e
ricardo <ricardo@inuits.eu>
2021-08-24 09:15:21 +0200

    Ref #81197, Adjusted text and styling as in given feedback

--- src/components/semantic/ImportantMessage.vue
+++ src/components/semantic/ImportantMessage.vue
@@ -2,7 +2,7 @@
   <div class="flex smo:flex-col py-3">
     <div>
       <strong class="text-sm"><i>Feedback</i></strong>
-      <div class="bg-white border-lightRed border-2 text-black px-2 py-1 rounded relative" role="alert">
+      <div class="bg-lightRed border-lightRed border-2 text-black px-2 py-1 rounded relative" role="alert">
         <span class="block sm:inline">{{ message }}</span>
         <span class="absolute top-0 bottom-0 right-0 px-4 py-3"> </span>
       </div>
