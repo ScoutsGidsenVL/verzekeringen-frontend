1678e6ec5e2fec82d647e1e0d13438bd0d698a67
ricardo <ricardo@inuits.eu>
2021-10-08 13:17:54 +0200

    Ref #80001, Implemented form data with claim and file

--- src/repositories/claims/claimRepository.ts
+++ src/repositories/claims/claimRepository.ts
@@ -1,4 +1,4 @@
-import { ClaimDeserializer, ClaimSerializer } from '@/serializer/claims/claim'
+import { Claim, ClaimDeserializer, ClaimSerializer } from '@/serializer/claims/claim'
 import { BaseRepository } from '@/repositories/baseRepository'
 import { ArrayResult } from '@/serializer/ArrayResult'
 
@@ -31,6 +31,50 @@ export class ClaimRepository extends BaseRepository {
       response.results = array
 
       return response
+    })
+  }
+
+  create(data: any, file?: any) {
+    const claim: Claim = this.serializer(data)
+    const fd = new FormData()
+
+    console.log('CHECK: ', this.serializer(data))
+
+    // if (claim.activityTypes) {
+    //   fd.append('activity_type', claim.activityTypes.toString())
+    // }
+
+    // if (claim.group && claim.group.name) {
+    //   console.log('GROUP FD: ', claim.group.name)
+    //   fd.append('group', claim.group.name)
+    // }
+
+    // if (claim.dateOfAccident) {
+    //   fd.append('date_of_accident', claim.dateOfAccident.toString())
+    // }
+
+    // if (claim.activity) {
+    //   fd.append('activity', claim.activity.toString())
+    // }
+
+    // if (claim.description) {
+    //   fd.append('description', claim.description.toString())
+    // }
+
+    fd.append('detail', this.serializer(data))
+
+    if (file) {
+      fd.append('file', file)
+    }
+
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    }
+
+    return this.post(this.endpoint, fd, config).then((response: any) => {
+      return this.deserializer(response)
     })
   }
 }
