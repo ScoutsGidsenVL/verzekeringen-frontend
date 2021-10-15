6847c0377e57a9bacc25560b776effbb1a57b881
ricardo <ricardo@inuits.eu>
2021-10-15 09:41:22 +0200

    Ref #81235, Adjusted text to only upload certain file types

--- src/components/semantic/FileUpload.vue
+++ src/components/semantic/FileUpload.vue
@@ -13,7 +13,8 @@
               <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
             </svg>
             <span class="mt-2 text-base leading-normal">{{!selectedFile ? 'Selecteer bestand' : 'Vervang bestand'}}</span>
-            <span>Enkel ..., <strong>MAX 5MB</strong></span>
+            <span class="text-center mt-2">Enkel pdf, jpg, jpeg, png, webp. <strong>MAX 5MB</strong></span>
+            
             <input @change="selectFile($event)" type="file" name="file" id="file" class="hidden" />
           </label>
         </div>
