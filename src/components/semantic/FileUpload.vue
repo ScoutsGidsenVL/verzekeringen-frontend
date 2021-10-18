ff6f2d72bd1b4c1a76202eb199e539f6771cd797
ricardo <ricardo@inuits.eu>
2021-10-18 10:01:21 +0200

    Ref #84144, Empty input field by getting element by id

--- src/components/semantic/FileUpload.vue
+++ src/components/semantic/FileUpload.vue
@@ -13,7 +13,7 @@
               <path d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
             </svg>
             <span class="mt-2 text-base leading-normal">{{!selectedFile ? 'Selecteer bestand' : 'Vervang bestand'}}</span>            
-            <input @change="selectFile($event)" type="file" name="file" id="file" class="hidden" />
+            <input ref="file" @change="selectFile($event)" type="file" name="file" id="file" class="hidden" />
           </label>
         </div>
           <span class="text-xs">Enkel pdf, jpg, jpeg, png, webp. <strong>MAX 5MB</strong></span>
@@ -24,7 +24,7 @@
       <div v-if="!selectedFile">
         <i>Geen bijlage geselecteerd</i>
       </div>
-      <div class="w-72" v-else>
+      <div class="w-72" v-if="selectedFile">
         <file-item-component v-on:deleteFile="deleteFile($event)" :file="selectedFile" :isDetailView="isDetailView" />
         <span :name="id">
           <ErrorMessage :name="'file'" class="text-red text-sm block w-80" />
@@ -35,9 +35,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, watch } from 'vue'
+import { defineComponent } from 'vue'
 import FileItemComponent from '@/components/semantic/FileItemComponent.vue'
-import { FileItem } from '@/serializer/FileItem'
 import { ErrorMessage, useField } from 'vee-validate'
 
 export default defineComponent({
@@ -47,27 +46,21 @@ export default defineComponent({
     ErrorMessage,
   },
   props: {},
-  setup(props, context) {
-    const files = ref<FileItem[]>([])
+  setup() {
 
     const { value: selectedFile } = useField<any>('file', 'fileSize', {})
 
     const deleteFile = () => {
       selectedFile.value = undefined
+      //@ts-ignore
+      document.getElementById('file').value = ''
     }
     const selectFile = (data: any) => {
+      console.log('SELECTED FILE: ', data.target.files[0])
       selectedFile.value = data.target.files[0]
     }
 
-    watch(
-      () => files.value,
-      () => {
-        context.emit('addFiles', files.value)
-      }
-    )
-
     return {
-      files,
       deleteFile,
       selectFile,
       selectedFile,
